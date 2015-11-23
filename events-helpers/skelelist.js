b9a9ea8e0daa3f6b3feeafc80fddc0af02be03a8
cerealkillerway <cerealkillerway@gmail.com>
2015-11-23 06:17:34 +0100

    firs version

---
+++ events-helpers/skelelist.js
@@ -0,0 +1,65 @@
+skelelistGeneralHelpers = {
+    label: function(name, options) {
+        switch(options) {
+
+            default:
+            return TAPi18n.__(name + "_lbl");
+        }
+    },
+    field: function(name, data, schema) {
+        var fieldSchema = schema[name];
+        var lang = FlowRouter.getQueryParam("lang");
+        var result = {};
+        var value;
+        var listOptions = fieldSchema.__listView;
+        var link = schema.__listView.detailLink;
+
+        if (fieldSchema.i18n === undefined) {
+            if (data[lang] && data[lang][name]) {
+                value = data[lang][name];
+            }
+            else {
+                value = name + ' (' + TAPi18n.__('translationTitleNoHTML_missing').toUpperCase() + ')';
+            }
+        }
+        else {
+            value = data[name];
+        }
+
+        // applies field's listview options
+        if (listOptions) {
+            if (listOptions.stripHTML) {
+                value = value.replace(/<(?:.|\n)*?>/gm, '');
+            }
+            if (listOptions.truncate) {
+                value = value.substr(0, listOptions.truncate);
+            }
+        }
+
+        // check if the field should be a link to detail page
+        if (link.params.indexOf(name) >= 0) {
+            var params = {};
+
+            link.params.forEach(function(param, index) {
+                params[param] = data[param];
+            });
+
+            result.link = FlowRouter.path(link.basePath, params, {lang: lang});
+        }
+
+        result.value = value;
+        return result;
+    }
+}
+
+
+// skelelist
+Template.skelelist.helpers({
+    listStyle: function(style) {
+        return 'skelelist' + style.capitalize();
+    }
+});
+
+
+// list table view
+Template.skelelistTable.helpers(skelelistGeneralHelpers);
---
+++ package.js
@@ -0,0 +1,93 @@
+Package.describe({
+    name: 'cerealkiller:skelelist',
+    version: '0.0.1',
+    // Brief, one-line summary of the package.
+    summary: 'collection views',
+    // URL to the Git repository containing the source code for this package.
+    git: '',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.2.1');
+
+    // packages
+    api.use('jquery', 'client');
+    api.use('underscore@1.0.0');
+    api.use('fourseven:scss@3.2.0', 'client');
+    api.use('tap:i18n@1.7.0');
+    api.use('blaze-html-templates', 'client');
+    api.use('materialize:materialize@0.97.3', 'client');
+    api.use('cerealkiller:utils');
+    // if skeletor is in use, load it before
+    api.use('cerealkiller:skeletor@0.0.1', {weak: true});
+
+    // exports
+    api.export('Skelelist');   // package namespace
+
+    // styles
+
+
+    // templates
+    api.addFiles('templates/skelelist.html', 'client');
+
+    // i18n
+    api.addFiles('i18n/it.i18n.json');
+    api.addFiles('i18n/en.i18n.json');
+
+    // libraries
+    api.addFiles('namespace.js');
+    api.addFiles('events-helpers/skelelist.js', 'client');
+    
+});
+
+Package.onTest(function(api) {
+    api.use('tinytest');
+    api.use('cerealkiller:skelelist');
+    api.addFiles('skeleform-tests.js');
+});
+
+
+
+
+// UTILITIES
+// get list of all files in a folder
+function getFilesFromFolder(packageName, folder){
+    // local imports
+    var _ = Npm.require("underscore");
+    var fs = Npm.require("fs");
+    var path = Npm.require("path");
+    // helper function, walks recursively inside nested folders and return absolute filenames
+    function walk(folder){
+        var filenames = [];
+        // get relative filenames from folder
+        var folderContent=fs.readdirSync(folder);
+        // iterate over the folder content to handle nested folders
+        _.each(folderContent,function(filename){
+            // build absolute filename
+            var absoluteFilename=folder + path.sep + filename;
+            // get file stats
+            var stat=fs.statSync(absoluteFilename);
+            if (stat.isDirectory()){
+                // directory case => add filenames fetched from recursive call
+                filenames=filenames.concat(walk(absoluteFilename));
+            }
+            else {
+                // file case => simply add it
+                filenames.push(absoluteFilename);
+            }
+        });
+        return filenames;
+    }
+    // save current working directory (something like "/home/user/projects/my-project")
+    var cwd = process.cwd();
+    // chdir to our package directory
+    process.chdir("packages" + path.sep + packageName);
+    // launch initial walk
+    var result = walk(folder);
+    // restore previous cwd
+    process.chdir(cwd);
+    return result;
+}
